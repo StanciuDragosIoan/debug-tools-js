1246a84337ed85504ca10547ce4a116311b42248
Dragos <Dragos.Dragos@honeywell.com>
2019-06-13 14:37:18 +0300

    test

---
+++ index.js
@@ -0,0 +1,11 @@
+// protect variable scope with an IIFE
+// (function() {
+//   var now = new Date();
+
+//   function doSomething() {
+//     alert("The time is " + now);
+//   }
+// })();
+
+//Breakpoints (standard vs conditional)
+//min 9:00
